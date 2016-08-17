8f344365bba42b8a7be368006f4a96848d5b3074
xyfhy <ljms8181@126.com>
2016-08-17 11:02:59 +0800

    add  user mapper

---
+++ spring_boot_mybatis/src/main/java/com/example/mappers/UserMapper.java
@@ -0,0 +1,21 @@
+package com.example.mappers;
+
+import com.example.entities.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by xyfhy on 16/8/17.
+ */
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM USER WHERE NAME = #{name}")
+    User findByName(@Param("name") String name);
+
+    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
+    int insert(@Param("name") String name, @Param("age") Integer age);
+}
